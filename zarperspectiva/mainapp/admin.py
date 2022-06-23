1370c9f9583646ff280383285fc4f935cf96e91e
tonych <antonlushnikow@gmail.com>
2022-06-23 12:42:12 +0500

    Add models Age, Course

--- zarperspectiva/mainapp/admin.py
+++ zarperspectiva/mainapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import SiteSettings, Subject, Teacher
+from .models import SiteSettings, Subject, Teacher, Age, Course
 
 admin.site.register(SiteSettings)
 admin.site.register(Subject)
 admin.site.register(Teacher)
+admin.site.register(Age)
+admin.site.register(Course)
--- zarperspectiva/mainapp/models.py
+++ zarperspectiva/mainapp/models.py
@@ -52,3 +52,69 @@ class Teacher(models.Model):
 
     def __str__(self):
         return f'{self.last_name} {self.first_name} {self.parent_name}'
+
+
+class Age(models.Model):
+    age = models.CharField(
+        verbose_name='возраст',
+        null=False,
+        max_length=30,
+    )
+
+
+class Course(models.Model):
+    title = models.CharField(
+        verbose_name='название курса',
+        max_length=100,
+        null=False,
+    )
+    info = models.TextField(
+        verbose_name='информация о курсе',
+        blank=True,
+    )
+    subject = models.ManyToManyField(
+        Subject,
+        verbose_name='предмет',
+    )
+    age = models.ManyToManyField(
+        Age,
+        verbose_name='возраст',
+    )
+    teacher = models.ForeignKey(
+        Teacher,
+        verbose_name='преподаватель',
+        on_delete=models.DO_NOTHING,
+    )
+    price_once_alone = models.IntegerField(
+        verbose_name='цена за занятие без группы',
+        default=600,
+    )
+    price_pass_alone = models.IntegerField(
+        verbose_name='цена за месяц без группы',
+        default=1600,
+    )
+    price_once_group = models.IntegerField(
+        verbose_name='цена за занятие без группы',
+        default=500,
+    )
+    price_pass_group = models.IntegerField(
+        verbose_name='цена за месяц без группы',
+        default=1400,
+    )
+    duration = models.IntegerField(
+        verbose_name='длительность занятия',
+        default=60,
+    )
+    count_a_week = models.IntegerField(
+        verbose_name='занятий в неделю',
+        default=1,
+    )
+    count_overall = models.IntegerField(
+        verbose_name='занятий всего',
+        default=10,
+    )
+    group_size = models.CharField(
+        verbose_name='размер группы',
+        blank=True,
+        max_length=30,
+    )
