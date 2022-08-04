1e52bc3916cb803998f5fed0ef54af55f164efb2
ash <ash@topdog.ru.net>
2022-08-04 10:26:11 +0300

    fix productions settings. rollback

--- zarperspectiva/zarperspectiva/settings.py
+++ zarperspectiva/zarperspectiva/settings.py
@@ -89,20 +89,14 @@ WSGI_APPLICATION = 'zarperspectiva.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "zarperspectiva",
-#         "USER": env("PSQL_USER"),
-#         "PASSWORD": env("PSQL_PASSWORD"),
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "zarperspectiva",
+        "USER": env("PSQL_USER"),
+        "PASSWORD": env("PSQL_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
